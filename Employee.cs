1561515d11d081c24a8cfc1ef0155ac36ffe3f43
William Berry <76190311+wberry86@users.noreply.github.com>
2021-11-08 21:00:47 -0600

    created employee constructor

---
+++ Employee.cs
@@ -0,0 +1,19 @@
+namespace CatWorx.BadgeMaker
+{
+    class Employee
+    {
+        public string FirstName;
+        public string LastName;
+        public int Id;
+        public string PhotoUrl;
+        public Employee(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public string GetName() {
+            return FirstName + " " + LastName;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace CatWorx.BadgeMaker
+{
+    class Program
+    {
+
+        static List<Employee> GetEmployees()
+        {
+
+
+            List<Employee> employees = new List<Employee>();
+            while (true)
+            {
+                Console.WriteLine("Please enter a name: (leave empty to exit): ");
+                string input = Console.ReadLine();
+                if (input == "")
+                {
+                    break;
+                }
+
+                // Create a new Employee instance
+                Employee currentEmployee = new Employee(input, "Smith");
+                employees.Add(currentEmployee);
+
+            }
+            // This is important!
+            return employees;
+        }
+
+        static void PrintEmployees(List<Employee> employees)
+        {
+            for (int i = 0; i < employees.Count; i++)
+            {
+                Console.WriteLine(employees[i].GetName());
+            }
+        }
+        static void Main(string[] args)
+        {
+            // This is our employee-getting code now
+            List<Employee> employees = GetEmployees();
+            PrintEmployees(employees);
+        }
+
+
+    }
+}
